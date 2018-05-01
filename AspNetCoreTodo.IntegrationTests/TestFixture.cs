afe9cfc35a92192322a7e1686088f649160cccc5
Marcelo Magano <marcelo.magano99@gmail.com>
2018-05-01 09:49:49 +0100

    Testing

---
+++ AspNetCoreTodo.IntegrationTests/TestFixture.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Net.Http;
+
+namespace AspNetCoreTodo.IntegrationTests
+{
+    public class TestFixture : IDisposable
+    {
+        private readonly TestServer _server;
+        public HttpClient Client { get; }
+
+        public TestFixture()
+        {
+            var builder = new WebHostBuilder()
+                .UseStartup<AspNetCoreTodo.Startup>()
+                .ConfigureAppConfiguration((context, configBuilder) =>
+                {
+                    configBuilder.SetBasePath(Path.Combine(
+                        Directory.GetCurrentDirectory(), "..\\..\\..\\..\\AspNetCoreTodo"));
+
+                    configBuilder.AddJsonFile("appsettings.json");
+
+                    // Add fake configuration for Facebook middleware (to avoid startup errors)
+                    //configBuilder.AddInMemoryCollection(new Dictionary<string, string>()
+                    //{
+                    //    ["Facebook:AppId"] = "fake-app-id",
+                    //    ["Facebook:AppSecret"] = "fake-app-secret"
+                    //});
+                });
+            _server = new TestServer(builder);
+
+            Client = _server.CreateClient();
+            Client.BaseAddress = new Uri("http://localhost:5000");
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            _server.Dispose();
+        }
+    }
+}
